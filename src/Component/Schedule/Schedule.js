53ea55abb88537ff1955e8c6fffcdfd87eec4fbd
mynul <homeexercise8@gmail.com>
2022-10-19 19:32:33 +0600

    fix'

--- src/Component/Schedule/Schedule.js
+++ src/Component/Schedule/Schedule.js
@@ -6,56 +6,89 @@ import women from '../../Assests/background/Group 286.png'
 import leftArrow from '../../Assests/Icons/leftArrow.png'
 import rightArrow from '../../Assests/Icons/rightarrow.png'
 import '../../Css/Schedule.css'
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/bundle";
+
+
+import { Navigation } from "swiper";
 const Schedule = () => {
     return (
         <Container>
             <h3>Schedule</h3>
-            {/* <div className='circle'>
 
-            </div>
-            <div className='CircleWrapper'>
-                <div className='BgCircleImage'>
-
-
-                </div>
-                <div className='WomenImagebg'>
-                </div>
-                <img className='womenImage' src={women} alt="" />
-
-            </div> */}
             <Row xs={1} sm={1} md={1} lg={2} xl={2}>
                 <Col className="position-relative">
                     <img className='YogaBanner' src={women} alt="" />
                     <p className='Yogaday'>1</p>
                 </Col>
                 <Col>
-                    <div>
-                        <div className='d-flex justify-content-between mb-3'>
-                            <h2 className="ScheduleTittle">Day 1- Class </h2>
-                            <div >
-                                <img className='me-2' src={leftArrow} alt="" />
-                                <img className='ms-2' src={rightArrow} alt="" />
+                    <Swiper
+                        rewind={true}
+                        navigation={true}
+                        modules={[Navigation]}
+                        className="mySwiper"
+                              onSlideChange={() => console.log('slide change')}
+
+                    >
+                        <SwiperSlide className="w-100">
+                            <div>
+                            <div className='d-flex justify-content-between mb-3'>
+                                <h2 className="ScheduleTittle">Day 1- Class </h2>
+
                             </div>
+                            <div className='TimeWrapper'>
+                                <select className='Times'>
+                                    <option value='1'>week 1</option>
+                                    <option value='2'>week 2</option>
+                                    <option value='3'>week 3</option>
+                                </select>
+                            </div>
+                            <div className='ScheduleDesc mt-3'>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                                <a className="joinbtn">
+                                    Join Today.
+                                </a>
+                            </div>
+
                         </div>
-                        <div className='TimeWrapper'>
-                            <select className='Times'>
-                                <option value='1'>week 1</option>
-                                <option value='2'>week 2</option>
-                                <option value='3'>week 3</option>
-                            </select>
+                        </SwiperSlide>
+                        <SwiperSlide className="w-100">
+                            <div>
+                            <div className=' mb-3'>
+                                <h2 className="ScheduleTittle">Day 1- Class </h2>
+
+                            </div>
+                            <div className='TimeWrapper'>
+                                <select className='Times'>
+                                    <option value='1'>week 1</option>
+                                    <option value='2'>week 2</option>
+                                    <option value='3'>week 3</option>
+                                </select>
+                            </div>
+                            <div className='ScheduleDesc mt-3'>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                                <a className="joinbtn">
+                                    Join Today.
+                                </a>
+                            </div>
+
                         </div>
-                        <div className='ScheduleDesc mt-3'>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            </p>
-                            <a className="joinbtn">
-                            Join Today.
-                            </a>
-                        </div>
-                        
-                    </div>
+                        </SwiperSlide>
+
+                    </Swiper>
+
                 </Col>
+
+
             </Row>
         </Container>
 
--- src/Component/TeacherSection/TeacherSection.js
+++ src/Component/TeacherSection/TeacherSection.js
@@ -4,6 +4,14 @@ import manTeacher from '../../Assests/People/man.png'
 import '../../Css/TeacherSection.css'
 import leftArrow from '../../Assests/Icons/leftArrow.png'
 import rightArrow from '../../Assests/Icons/rightarrow.png'
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/bundle";
+import { Navigation } from "swiper";
+
 const TeacherSection = () => {
     const [state, setState] = useReducer(
         (state, newState) => ({ ...state, ...newState }),
@@ -23,36 +31,62 @@ const TeacherSection = () => {
                         <ul className="Teachers">
                             <li ><h5>Ashrei Mitra</h5></li>
                             <li ><h5>Rahul</h5></li>
-                            <li className={`${state.TeacherAshrei&&"TeacherList"}`} onClick={()=>setState({TeacherAshrei:true})}><h5>Ashrei Mitra</h5></li>
+                            <li className={`${state.TeacherAshrei && "TeacherList"}`} onClick={() => setState({ TeacherAshrei: true })}><h5>Ashrei Mitra</h5></li>
                             <li ><h5>Ashray</h5></li>
                             <li ><h5>Ashish</h5></li>
                             <li ><h5>Zora</h5></li>
                         </ul>
                     </div>
                 </Col>
-                <Col xs={12} sm={12} md={12} lg={8} className="TeacherDesc">
-                    <div className='d-flex justify-content-between gap-4'>
-                        <div>
-                            <div className="TeacherImageWrapper ">
-                                <img className="TeacherImage " src={manTeacher} alt="" />
-                            </div>
-                            <p className="text-center TeacherName">
-                                Ashrei Mitra
-                            </p>
-                        </div>
+                <Col xs={12} sm={12} md={12} lg={8} className="">
+                    <Swiper
+                        rewind={true}
+                        navigation={true}
+                        modules={[Navigation]}
+                        className="mySwiper1"
+                        onSlideChange={() => console.log('slide change')}
 
-                        <div className='d-flex align-items-center MainDescTeacher'>
-                            <p>
-                                Ashrei Mitra started his journey in yoga at the age of 16. He was chosen by his master Dr. Bharat Thakur from a meditation workshop and has worked with him closely for many years to become one of the most senior teachers at Bharat Thakur Artistic Yoga. Combining his love of fitness, breathwork, functional training, and his deep knowledge of the human anatomy and yogic knowhow, he is committed to maximizing on the human potential by strengthening and opening up the body so that the mind naturally falls into a deep state of relaxation and meditation. He has taught yoga to people from all walks of life including athletes, celebrities and several top corporate organizations
-                            </p>
-                        </div>
-                    </div>
-                    <div className='d-flex justify-content-end mt-5 mb-5'>
-                        <div >
-                            <img className='me-2' src={leftArrow} alt="" />
-                            <img className='ms-2' src={rightArrow} alt="" />
-                        </div>
-                    </div>
+                    >
+                        <SwiperSlide className="w-100">
+                            <div className='d-flex  justify-content-between gap-4 TeacherDesc'>
+                                <div>
+                                    <div className="TeacherImageWrapper ">
+                                        <img className="TeacherImage " src={manTeacher} alt="" />
+                                    </div>
+                                    <p className="text-center TeacherName">
+                                        Ashrei Mitra
+                                    </p>
+                                </div>
+
+                                <div className='d-flex align-items-center MainDescTeacher'>
+                                    <p>
+                                        Ashrei Mitra started his journey in yoga at the age of 16. He was chosen by his master Dr. Bharat Thakur from a meditation workshop and has worked with him closely for many years to become one of the most senior teachers at Bharat Thakur Artistic Yoga. Combining his love of fitness, breathwork, functional training, and his deep knowledge of the human anatomy and yogic knowhow, he is committed to maximizing on the human potential by strengthening and opening up the body so that the mind naturally falls into a deep state of relaxation and meditation. He has taught yoga to people from all walks of life including athletes, celebrities and several top corporate organizations
+                                    </p>
+                                </div>
+                            </div>
+
+                        </SwiperSlide>
+                        <SwiperSlide className="w-100">
+                            <div className='d-flex justify-content-between gap-4 TeacherDesc'>
+                                <div>
+                                    <div className="TeacherImageWrapper ">
+                                        <img className="TeacherImage " src={manTeacher} alt="" />
+                                    </div>
+                                    <p className="text-center TeacherName">
+                                        Ashrei Mitra
+                                    </p>
+                                </div>
+
+                                <div className='d-flex align-items-center MainDescTeacher'>
+                                    <p>
+                                        Ashrei Mitra started his journey in yoga at the age of 16. He was chosen by his master Dr. Bharat Thakur from a meditation workshop and has worked with him closely for many years to become one of the most senior teachers at Bharat Thakur Artistic Yoga. Combining his love of fitness, breathwork, functional training, and his deep knowledge of the human anatomy and yogic knowhow, he is committed to maximizing on the human potential by strengthening and opening up the body so that the mind naturally falls into a deep state of relaxation and meditation. He has taught yoga to people from all walks of life including athletes, celebrities and several top corporate organizations
+                                    </p>
+                                </div>
+                            </div>
+
+                        </SwiperSlide>
+                    </Swiper>
+
                 </Col>
             </Row>
         </Container>
